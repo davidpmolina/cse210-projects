17acf90ba9e76d10cd9e7a74a9c3f1bf2c3a94bd
davidpmolina <dmolina@byupathway.edu>
2025-03-17 23:09:06 +0800

    Fractions complete

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,34 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        
+        fraction1.SetTop(1);
+        fraction1.SetBottom(1);
+
+        fraction2.SetTop(5);
+        fraction2.SetBottom(1);
+
+        fraction3.SetTop(3);
+        fraction3.SetBottom(4);
+
+        Fraction fraction4 = new Fraction(1,3);
+
+        
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
     }
 }
